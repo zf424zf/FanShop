2961126b59327549826522e2f1a61d019123600f
zf424zf <704273241@qq.com>
2018-09-23 23:43:23 +0800

    add update & delete address and create policy

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -8,20 +8,31 @@ use Illuminate\Http\Request;
 
 class UserAddressesController extends Controller
 {
-    //
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index(Request $request)
     {
         //将登录用户地址注入view
-        return view('user_addresses.index',[
-            'addresses'=> $request->user()->addresses
+        return view('user_addresses.index', [
+            'addresses' => $request->user()->addresses
         ]);
     }
 
+    /**
+     * @param UserAddress $userAddress
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create(UserAddress $userAddress)
     {
         return view('user_addresses.create_and_edit', ['address' => $userAddress]);
     }
 
+    /**
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(UserAddressRequest $request)
     {
         $request->user()->addresses()->create($request->only([
@@ -35,5 +46,50 @@ class UserAddressesController extends Controller
         ]));
 
         return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * @param UserAddress $user_address
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+    /**
+     * @param UserAddress $user_address
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * @param UserAddress $user_address
+     * @return array
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->delete();
+        return [];
     }
 }
