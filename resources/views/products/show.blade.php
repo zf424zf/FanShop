a09ec43f4607859894dc8068d90be4c17a5ec4e1
zf424zf <704273241@qq.com>
2018-09-30 22:56:16 +0800

    add cart service

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -15,8 +15,11 @@
                             <div class="price"><label>价格</label><em>￥</em><span>{{ $product->price }}</span></div>
                             <div class="sales_and_reviews">
                                 <div class="sold_count">累计销量 <span class="count">{{ $product->sold_count }}</span></div>
-                                <div class="review_count">累计评价 <span class="count">{{ $product->review_count }}</span></div>
-                                <div class="rating" title="评分 {{ $product->rating }}">评分 <span class="count">{{ str_repeat('★', floor($product->rating)) }}{{ str_repeat('☆', 5 - floor($product->rating)) }}</span></div>
+                                <div class="review_count">累计评价 <span class="count">{{ $product->review_count }}</span>
+                                </div>
+                                <div class="rating" title="评分 {{ $product->rating }}">评分 <span
+                                            class="count">{{ str_repeat('★', floor($product->rating)) }}{{ str_repeat('☆', 5 - floor($product->rating)) }}</span>
+                                </div>
                             </div>
                             <div class="skus">
                                 <label>选择</label>
@@ -29,12 +32,15 @@
                                                 data-toggle="tooltip"
                                                 title="{{ $sku->description }}"
                                                 data-placement="bottom">
-                                            <input type="radio" name="skus" autocomplete="off" value="{{ $sku->id }}"> {{ $sku->title }}
+                                            <input type="radio" name="skus" autocomplete="off"
+                                                   value="{{ $sku->id }}"> {{ $sku->title }}
                                         </label>
                                     @endforeach
                                 </div>
                             </div>
-                            <div class="cart_amount"><label>数量</label><input type="text" class="form-control input-sm" value="1"><span>件</span><span class="stock"></span></div>
+                            <div class="cart_amount"><label>数量</label><input type="text" class="form-control input-sm"
+                                                                             value="1"><span>件</span><span
+                                        class="stock"></span></div>
                             <div class="buttons">
                                 @if($favored)
                                     <button class="btn btn-danger btn-disfavor">取消收藏</button>
@@ -47,8 +53,11 @@
                     </div>
                     <div class="product-detail">
                         <ul class="nav nav-tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#product-detail-tab" aria-controls="product-detail-tab" role="tab" data-toggle="tab">商品详情</a></li>
-                            <li role="presentation"><a href="#product-reviews-tab" aria-controls="product-reviews-tab" role="tab" data-toggle="tab">用户评价</a></li>
+                            <li role="presentation" class="active"><a href="#product-detail-tab"
+                                                                      aria-controls="product-detail-tab" role="tab"
+                                                                      data-toggle="tab">商品详情</a></li>
+                            <li role="presentation"><a href="#product-reviews-tab" aria-controls="product-reviews-tab"
+                                                       role="tab" data-toggle="tab">用户评价</a></li>
                         </ul>
                         <div class="tab-content">
                             <div role="tabpanel" class="tab-pane active" id="product-detail-tab">
@@ -79,12 +88,12 @@
                             .then(function () {
                                 location.reload();
                             });
-                    }, function(error) {
+                    }, function (error) {
                         if (error.response && error.response.status === 401) {
                             swal('请先登录', '', 'error');
-                        }  else if (error.response && error.response.data.msg) {
+                        } else if (error.response && error.response.data.msg) {
                             swal(error.response.data.msg, '', 'error');
-                        }  else {
+                        } else {
                             swal('系统错误', '', 'error');
                         }
                     });
@@ -99,7 +108,40 @@
                             });
                     });
             });
+                // 加入购物车按钮点击事件
+            $('.btn-add-to-cart').click(function () {
 
+                // 请求加入购物车接口
+                axios.post('{{ route('cart.add') }}', {
+                    sku_id: $('label.active input[name=skus]').val(),
+                    amount: $('.cart_amount input').val(),
+                })
+                    .then(function () { // 请求成功执行此回调
+                        swal('加入购物车成功', '', 'success');
+                    }, function (error) { // 请求失败执行此回调
+                        if (error.response.status === 401) {
+
+                            // http 状态码为 401 代表用户未登陆
+                            swal('请先登录', '', 'error');
+
+                        } else if (error.response.status === 422) {
+
+                            // http 状态码为 422 代表用户输入校验失败
+                            var html = '<div>';
+                            _.each(error.response.data.errors, function (errors) {
+                                _.each(errors, function (error) {
+                                    html += error + '<br>';
+                                })
+                            });
+                            html += '</div>';
+                            swal({content: $(html)[0], icon: 'error'})
+                        } else {
+
+                            // 其他情况应该是系统挂了
+                            swal('系统错误', '', 'error');
+                        }
+                    })
+            });
         });
     </script>
 @endsection
