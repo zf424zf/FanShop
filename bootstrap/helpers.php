8f9417b8940f18e61aadf3e300e6f011b742b599
zf424zf <704273241@qq.com>
2018-10-17 12:06:49 +0800

    初始化分期付款模型

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -8,3 +8,8 @@ function route_class()
 {
     return str_replace('.','-',Route::currentRouteName());
 }
+// 默认的精度为小数点后两位
+function big_number($number, $scale = 2)
+{
+    return new \Moontoast\Math\BigNumber($number, $scale);
+}
