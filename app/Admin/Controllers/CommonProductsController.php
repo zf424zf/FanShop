c031232a903dd9bee7e469c6183d55f896e33688
zf424zf <704273241@qq.com>
2018-10-27 23:49:46 +0800

    同步商品数据至 Elasticsearch

--- app/Admin/Controllers/CommonProductsController.php
+++ app/Admin/Controllers/CommonProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Jobs\SyncOneProductToES;
 use App\Models\Category;
 use Encore\Admin\Grid;
 use Encore\Admin\Form;
@@ -97,6 +98,10 @@ abstract class CommonProductsController extends Controller
 
         $form->saving(function (Form $form) {
             $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+        });
+        $form->saved(function (Form $form) {
+            $product = $form->model();
+            $this->dispatch(new SyncOneProductToES($product));
         });
 
         return $form;
